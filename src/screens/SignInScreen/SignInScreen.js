b3bdcbb47fbc6b4a15d60d44c15429e0a3385524
Ian Kaire <iankaire@gmail.com>
2024-05-20 12:58:31 +0300

    created CustomInput component

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,10 +1,15 @@
 
-import React from 'react';
-import { View, Text, StyleSheet, Image, useWindowDimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, useWindowDimensions } from 'react-native';
 import Logo from '../../../assets/images/Logo_1.png';
+import CustomInput from '../../components/CustomInput';
 
 const SignInScreen = () => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
     const {height} = useWindowDimensions();
+
     return (
         <View style={styles.root}>
              <Image
@@ -12,6 +17,8 @@ const SignInScreen = () => {
                 style={[styles.logo, {height: height * 0.3}]}
                 resizeMode="contain"
         />
+        <CustomInput placeholder="Username" value={username} setValue={setUserName} secureTextEntry={false}/>
+        <CustomInput placeholder="Password" value={password} setValue={setPassword} secureTextEntry={true}/>
         </View>
     );
 };
