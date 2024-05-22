ed4fbcd17b2a7e841b1d7c17b8c9979f0ebb4c39
Ian Kaire <iankaire@gmail.com>
2024-05-22 12:54:27 +0300

    configured amplify

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import {
   Text,
 } from 'react-native';
 import Navigation from './src/navigation';
+import Amplify from 'aws-amplify';
+import config from './src/aws-exports';
+
+Amplify.configure(config);
 
 const App = () => {
   return (
