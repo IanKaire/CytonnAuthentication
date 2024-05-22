774d4a90989b16859217478fcd272a998b63f886
Ian Kaire <iankaire@gmail.com>
2024-05-22 14:24:08 +0300

    implemented aws amplify authentication

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 import Navigation from './src/navigation';
 import Amplify from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react-native';
 import config from './src/aws-exports';
 
 Amplify.configure(config);
@@ -25,4 +26,41 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+const signUpConfig = {
+  header: "My Customized Sign Up",
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Full name",
+      key: "name",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+    },
+    {
+      label: "Username",
+      key: "preferred_username",
+      required: true,
+      displayOrder: 3,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 4,
+      type: "password",
+    },
+  ],
+};
+
+export default withAuthenticator(App, {signUpConfig});
+
+
