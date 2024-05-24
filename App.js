c349b585c7ab1780e2e7b995ad63333f879dba74
Ian Kaire <iankaire@gmail.com>
2024-05-24 16:07:12 +0300

    added splashscreen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,10 +8,16 @@ import {
 import Navigation from './src/navigation';
 import Amplify from 'aws-amplify';
 import config from './src/aws-exports';
+import SplashScreen from 'react-native-splash-screen';
 
 Amplify.configure(config);
 
 const App = () => {
+
+  useEffect(() => {
+     if(Platform.OS === 'android') SplashScreen.hide();
+  },[]);
+  
   // Auth.signOut();
   return (
     <SafeAreaView style={styles.root}>
